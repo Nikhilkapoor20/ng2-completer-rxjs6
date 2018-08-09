156df4935760885fe8b67c17d5d662c5b2c877b7
Nikhil <nikhil.kapoor@rsystems.com>
2018-08-09 11:06:51 +0530

    ng2-completer-with-rxjs6 support

---
+++ demo/custom-data.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+
+import { CompleterData, CompleterItem } from "../src/ng2-completer";
+import { map } from "rxjs/operators";
+
+export class CustomData extends Subject<CompleterItem[]> implements CompleterData {
+    constructor(private http: HttpClient) {
+        super();
+    }
+    public search(term: string): void {
+        this.http.get("http://mysafeinfo.com/api/data?list=seinfeldepisodes&format=json&nm=" + term + ",contains")
+            .pipe(
+                map((data: any) => {
+                    let matches = (<Array<any>>data).map((episode: any) => this.convertToItem(episode)).filter(episode => !!episode) as CompleterItem[];
+                    this.next(matches);
+                })
+            )
+            .subscribe();
+    }
+
+    public cancel() {
+        // Handle cancel
+    }
+
+    public convertToItem(data: any): CompleterItem | null {
+        if (!data) {
+            return null;
+        }
+        // data will be string if an initial value is set
+        return {
+            title: typeof data === "string" ? data : data.nm
+        } as CompleterItem;
+    }
+}
